a12c4c1810c17035038ae2d9a7445b09414df1b2
levanvux <hoanngu512@gmail.com>
2026-04-10 02:01:06 +0700

    chore: log which port does server run on

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
 }
 bootstrap();

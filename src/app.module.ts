978bf734bd7827849fd2ce00b1c27b0648a427e3
levanvux <hoanngu512@gmail.com>
2026-04-10 05:56:06 +0700

    remove app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
@@ -36,7 +34,5 @@ import { AuthModule } from './auth/auth.module';
     OrdersModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

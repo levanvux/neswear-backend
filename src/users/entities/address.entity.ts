e2a8f6c9cabf301cf503e22615a94247ab1d50ab
levanvux <hoanngu512@gmail.com>
2026-06-26 13:20:36 +0700

    refactor(users & addresses): improve DTO & entity

--- src/users/entities/address.entity.ts
+++ src/users/entities/address.entity.ts
@@ -1,18 +1,26 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty } from 'class-validator';
 import { User } from './user.entity';
 
-@Entity()
+@Entity('addresses')
 export class Address {
   @PrimaryGeneratedColumn()
   id!: number;
 
   @Column()
+  @IsNotEmpty()
   street!: string;
 
   @Column()
+  @IsNotEmpty()
+  ward!: string;
+
+  @Column()
+  @IsNotEmpty()
   district!: string;
 
   @Column()
+  @IsNotEmpty()
   city!: string;
 
   @ManyToOne(() => User, (user) => user.addresses)

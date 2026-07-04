62d153c43cc11db0a8ea89aeb26d0329bfc5c556
levanvux <hoanngu512@gmail.com>
2026-07-04 14:06:48 +0700

    feat: add global /api prefix for all routes

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-   app.setGlobalPrefix("api");
+  app.setGlobalPrefix('api');
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
